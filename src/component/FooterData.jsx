ad4caa849e8e39f3efb18ba0dc1134e1e76aada5
maichien1232001 <taisaolaibiet@gmail.com>
2022-12-21 05:36:13 +0700

    Create FooterData.jsx

---
+++ src/component/FooterData.jsx
@@ -0,0 +1,24 @@
+import { faMobileAlt, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBuffer } from '@fortawesome/free-brands-svg-icons'
+
+export const usefulLink = [
+    {name: 'Home', id: 1},
+    {name: 'About us', id: 2},
+    {name: 'Services', id: 3},
+    {name: 'Team', id: 4},
+    {name: 'Blog', id: 5}
+]
+export const ourServices = [
+    {name: 'Strategy & Research', id: 6},
+    {name: 'Web Design', id: 7},
+    {name: 'Web Development', id: 8},
+    {name: 'Digital Marketing', id: 9},
+    {name: 'Graphic Design', id: 10}
+]
+export const otherLinks = [
+    {name: 'FAQ', id: 11},
+    {name: 'Portfolio', id: 12},
+    {name: 'Privacy Policy', id: 13},
+    {name: 'Terms & Conditions', id: 14},
+    {name: 'Support', id: 15},
+]
