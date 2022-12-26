ffc25883a02ae86407f1c09d600064e5441d4597
maichien1232001 <taisaolaibiet@gmail.com>
2022-12-26 09:49:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import React, { createContext, useState } from 'react'
 import About from './component/Home/About/About';
-export const UserContext = createContext();
+
 
 const App = () => {
-  const [admin, setAdmin] = useState(false);
-  const [selectedService, setSelectedService] = useState({})
   return (
-    <UserContext.Provider value={{ admin, setAdmin, selectedService, setSelectedService }}>
+
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
-    </UserContext.Provider>
   )
 }
 
